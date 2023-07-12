ba7e1cfdd91dd57ac368a958db54223afe61e781
chilleno <antonion.gonzalezf@gmail.com>
2023-07-12 19:58:39 -0400

    Update Logo.tsx
    - add animations

--- src/app/components/Logo/Logo.tsx
+++ src/app/components/Logo/Logo.tsx
@@ -3,21 +3,21 @@ export default function Logo() {
         <div>
             <div className='flex flex-row justify-center content-center'>
                 <img
-                    className='h-[36px] w-[28px] absolute mr-[350px] mt-16'
+                    className='h-[36px] w-[28px] absolute mr-[350px] mt-16 animate-[wiggle_.5s_ease-in-out_infinite]'
                     src="/logo/star.png" />
                 <img
-                    className='h-[193px] w-[387px]'
+                    className='h-[193px] w-[387px] animate-[wiggle_10s_ease-in-out_infinite]'
                     src="/logo/title.png" />
                 <img
-                    className='h-[36px] w-[28px] absolute mt-5 ml-[330px]'
+                    className="h-[36px] w-[28px] absolute mt-5 ml-[330px] animate-[wiggle_.5s_ease-in-out_infinite]"
                     src="/logo/star.png" />
             </div>
             <div className='flex flex-row gap-20 mt-4 justify-center content-center'>
                 <img
-                    className='h-[198px] w-[169px]'
+                    className="h-[198px] w-[169px] animate-[wiggle_3s_ease-in-out_infinite]"
                     src="/logo/claudio.png" />
                 <img
-                    className='h-[205px] w-[170px]'
+                    className="h-[205px] w-[170px] animate-[wiggle_3s_ease-in-out_infinite]"
                     src="/logo/lucho.png" />
             </div>
         </div>
