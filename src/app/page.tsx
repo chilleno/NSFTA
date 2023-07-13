1b259e533a57eae9af2c07539e78d8fb754c16c5
chilleno <antonion.gonzalezf@gmail.com>
2023-07-13 19:44:09 -0400

    Update page.tsx
    - import data from json
    - handle data from  state
    - import chapterLeft component
    - import chapterRight component
    - use chapterLeft component
    - use chapterRight component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 "use client"
 import React, { useEffect, useRef, useState } from 'react';
-import Logo from './components/Logo/Logo';
 import { SpeakerWaveIcon, SpeakerXMarkIcon } from '@heroicons/react/24/solid';
+import Logo from './components/Logo/Logo';
+import data from './data.json';
+import ChapterLeft from './components/ChapterLeft/ChapterLeft';
+import ChapterRight from './components/ChapterRight/ChapterRight';
 
 export default function Home() {
   const [audioEnabled, setAudioEnabled] = useState<boolean>(false);
+  const [chapterList, setChapterList] = useState<any>(data.chapters);
   let audio: HTMLAudioElement | null = null;
 
   useEffect(() => {
@@ -26,34 +30,65 @@ export default function Home() {
   const toggleAudio = () => {
     setAudioEnabled(prevState => !prevState);
   };
+
+  function isEven(number: number): boolean {
+    return number % 2 === 0;
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
-      <div className="z-10 w-full items-center justify-center font-mono text-sm flex">
-        <div className="w-1/6">
-          <div className="absolute top-20 left-20">
-            <h1 className="text-5xl">0/10</h1>
+      <div className="z-10 w-full items-center justify-center font-mono text-sm flex flex-col">
+        <div className="z-10 w-full items-center justify-center flex">
+          <div className="w-1/6">
+            <div className="absolute top-20 left-20">
+              <h1 className="text-5xl">0/10</h1>
+            </div>
           </div>
-        </div>
-        <div className="w-4/6">
-          <Logo />
-        </div>
-        <div className="w-1/6">
-          <div className="absolute top-20 right-20">
-            {
-              audioEnabled ?
-                <>
-                  <SpeakerWaveIcon
+          <div className="w-4/6">
+            <Logo />
+          </div>
+          <div className="w-1/6">
+            <div className="absolute top-20 right-20">
+              {
+                audioEnabled ?
+                  <>
+                    <SpeakerWaveIcon
+                      onClick={toggleAudio}
+                      className="h-10 w-10 text-blue-500 hover:cursor-pointer"
+                    />
+                  </>
+                  :
+                  <SpeakerXMarkIcon
                     onClick={toggleAudio}
                     className="h-10 w-10 text-blue-500 hover:cursor-pointer"
                   />
-                </>
-                :
-                <SpeakerXMarkIcon
-                  onClick={toggleAudio}
-                  className="h-10 w-10 text-blue-500 hover:cursor-pointer"
-                />
-            }
+              }
+            </div>
           </div>
+        </div>
+        <div className="z-10 w-full items-center justify-center flex flex-col mt-32">
+          {
+            chapterList && chapterList.map((chapter: any, index: number) => (
+              isEven(index) ?
+                <ChapterLeft
+                  key={'chapter_' + index}
+                  id={chapter.id}
+                  name={chapter.name}
+                  url={chapter.video_url}
+                  haveWord={chapter.have_word}
+                  word={chapter.word}
+                />
+                :
+                <ChapterRight
+                  key={'chapter_' + index}
+                  id={chapter.id}
+                  name={chapter.name}
+                  url={chapter.video_url}
+                  haveWord={chapter.have_word}
+                  word={chapter.word}
+                />
+            ))
+          }
         </div>
       </div>
     </main>
