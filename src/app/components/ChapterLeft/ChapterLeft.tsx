f5214133fc09f7f7965337f1379f50eef9a8e2cf
chilleno <antonion.gonzalezf@gmail.com>
2023-07-20 14:10:14 -0400

    Update ChapterLeft.tsx
    - add md responsive

--- src/app/components/ChapterLeft/ChapterLeft.tsx
+++ src/app/components/ChapterLeft/ChapterLeft.tsx
@@ -65,8 +65,8 @@ const ChapterLeft: React.FC<TaskListProps> = ({ id, name, url, haveWord, word, s
                 />
             </div>
             <div className="flex w-1/2 content-center justify-center">
-                <div className={haveWord ? "flex flex-col w-2/4 px-2 py-6" : "flex flex-col w-2/4 px-2 py-2 mt-24"}>
-                    <div className={haveWord ? "bubble grow right min-w-full" : "min-w-full bubble grow right xl:max-h-16 lg:max-h-24"}>
+                <div className={haveWord ? "flex flex-col w-2/4 px-2 py-6 md:-mt-4" : "flex flex-col w-2/4 px-2 py-2 xl:mt-24 lg:mt-24 md:mt-10"}>
+                    <div className={haveWord ? "bubble grow right min-w-full" : "min-w-full bubble grow right xl:max-h-16 lg:max-h-24 md:max-h-24"}>
                         <div className={haveWord ? "font-outfit text-2xl mt-5" : "font-outfit text-2xl mt-2"}>
                             <b>{name}</b>
                         </div>
@@ -77,7 +77,7 @@ const ChapterLeft: React.FC<TaskListProps> = ({ id, name, url, haveWord, word, s
                             {
                                 !wordMatch && haveWord &&
                                 <input
-                                    className={`border-b-2 ${showError && 'shake-error'}`}
+                                    className={`md:max-w-[180px] border-b-2 ${showError && 'shake-error'}`}
                                     placeholder="??????????????????"
                                     type="text"
                                     onChange={(e) => setCurrentTry(e.target.value)}
@@ -101,7 +101,7 @@ const ChapterLeft: React.FC<TaskListProps> = ({ id, name, url, haveWord, word, s
                         </div>
                     </div>
                 </div>
-                <div className="flex w-2/6 mt-16 xl:ml-28 lg:ml-28">
+                <div className="flex w-2/6 mt-16 xl:ml-28 lg:ml-28 md:ml-16">
                     <img
                         className="h-[198px] w-[169px] animate-[wiggle_3s_ease-in-out_infinite]"
                         src="/logo/lucho.png" />
