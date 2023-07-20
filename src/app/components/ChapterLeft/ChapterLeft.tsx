cac00a73a10af5f926f18411c4ac02a783933233
chilleno <antonion.gonzalezf@gmail.com>
2023-07-20 18:51:54 -0400

    Update ChapterLeft.tsx
    - update responsive
    - add mobile view

--- src/app/components/ChapterLeft/ChapterLeft.tsx
+++ src/app/components/ChapterLeft/ChapterLeft.tsx
@@ -54,19 +54,11 @@ const ChapterLeft: React.FC<TaskListProps> = ({ id, name, url, haveWord, word, s
     }, [])
 
     return (
-        <div className="w-full flex flex-row mt-24" id={'chapter_' + id}>
-            <div className="flex w-1/2 content-center justify-center">
-                <iframe
-                    className="border-white border-4"
-                    width="600"
-                    height="335"
-                    src={url}
-                    allowFullScreen
-                />
-            </div>
-            <div className="flex w-1/2 content-center justify-center">
-                <div className={haveWord ? "flex flex-col w-2/4 px-2 py-6 md:-mt-4" : "flex flex-col w-2/4 px-2 py-2 xl:mt-24 lg:mt-24 md:mt-10"}>
-                    <div className={haveWord ? "bubble grow right min-w-full" : "min-w-full bubble grow right xl:max-h-16 lg:max-h-24 md:max-h-24"}>
+        <div className="flex xl:flex-row lg:flex-row md:flex-row sm:flex-col mt-12 max-w-screen w-full" id={'chapter_' + id}>
+            <iframe src={url} allowFullScreen className="flex xl:w-1/2 lg:w-1/2 md:w-1/2 sm:w-[320px] xl:ml-0 lg:ml-0 md:ml-0 sm:ml-4 content-center justify-center border-white border-4" />
+            <div className="flex xl:w-1/2 lg:w-1/2 md:w-1/2 sm:w-[320px] content-center justify-center">
+                <div className={haveWord ? "flex flex-col w-4/6 max-w-4/6 px-2 py-6 md:-mt-4" : "flex flex-col w-4/6 max-w-4/6 px-2 py-2 xl:mt-24 lg:mt-24 md:mt-10"}>
+                    <div className={haveWord ? "bubble grow right min-w-full" : "min-w-full bubble grow right xl:max-h-24 lg:max-h-24 md:max-h-24"}>
                         <div className={haveWord ? "font-outfit text-2xl mt-5" : "font-outfit text-2xl mt-2"}>
                             <b>{name}</b>
                         </div>
@@ -77,7 +69,7 @@ const ChapterLeft: React.FC<TaskListProps> = ({ id, name, url, haveWord, word, s
                             {
                                 !wordMatch && haveWord &&
                                 <input
-                                    className={`md:max-w-[180px] border-b-2 ${showError && 'shake-error'}`}
+                                    className={`md:max-w-[150px] sm:max-w-[100px] border-b-2 ${showError && 'shake-error'}`}
                                     placeholder="??????????????????"
                                     type="text"
                                     onChange={(e) => setCurrentTry(e.target.value)}
@@ -101,9 +93,9 @@ const ChapterLeft: React.FC<TaskListProps> = ({ id, name, url, haveWord, word, s
                         </div>
                     </div>
                 </div>
-                <div className="flex w-2/6 mt-16 xl:ml-28 lg:ml-28 md:ml-16">
+                <div className="flex w-2/6 xl:ml-12 lg:ml-12 md:ml-16 xl:mt-16 lg:mt-16 md:mt-16 sm:mt-8 sm:ml-10">
                     <img
-                        className="h-[198px] w-[169px] animate-[wiggle_3s_ease-in-out_infinite]"
+                        className="xl:h-[198px] lg:h-[198px] md:h-[198px] xl:w-[169px] lg:w-[169px] md:w-[169px] sm:absolute sm:w-[90px] sm:h-[120px] animate-[wiggle_3s_ease-in-out_infinite]"
                         src="/logo/lucho.png" />
                 </div>
             </div>
