67f0a62a18faab5c7ee11acac2fd4c6f71463a1d
chilleno <antonion.gonzalezf@gmail.com>
2023-07-20 19:34:57 -0400

    Update ChapterRight.tsx
    -fix responsive

--- src/app/components/ChapterRight/ChapterRight.tsx
+++ src/app/components/ChapterRight/ChapterRight.tsx
@@ -61,7 +61,7 @@ const ChapterRight: React.FC<TaskListProps> = ({ id, name, url, haveWord, word, 
                         className="h-[198px] w-[169px] animate-[wiggle_3s_ease-in-out_infinite]"
                         src="/logo/claudio.png" />
                 </div>
-                <div className={haveWord ? "flex flex-col w-4/6 max-w-4/6 px-2 py-6 md:-mt-4" : "flex flex-col w-4/6 max-w-4/6 px-2 py-2 xl:mt-24 lg:mt-24 md:mt-10"}>
+                <div className={haveWord ? "flex flex-col w-4/6 max-w-4/6 px-2 py-6" : "flex flex-col w-4/6 max-w-4/6 px-2 py-2"}>
                     <div className={haveWord ? "bubble grow left max-w-full" : "max-w-full bubble grow left xl:max-h-24 lg:max-h-24 md:max-h-24"}>
                         <div className={haveWord ? "font-outfit text-2xl mt-5" : "font-outfit text-2xl mt-2"}>
                             <b>{name}</b>
@@ -98,7 +98,7 @@ const ChapterRight: React.FC<TaskListProps> = ({ id, name, url, haveWord, word, 
                     </div>
                 </div>
             </div>
-            <iframe src={url} allowFullScreen className="flex xl:w-1/2 lg:w-1/2 md:w-1/2 sm:w-[320px] xl:ml-0 lg:ml-0 md:ml-0 sm:ml-4 content-center justify-center border-white border-4" />
+            <iframe src={url} allowFullScreen className="flex xl:w-1/2 lg:w-1/2 md:w-1/2 sm:w-[320px] xl:ml-0 lg:ml-0 md:ml-0 sm:ml-4 content-center justify-center border-white border-4 h-[310px]" />
             <div className="content-center justify-center xl:hidden lg:hidden md:hidden sm:flex flex-row">
                 <div className={haveWord ? "flex flex-col w-4/6 px-2 py-6 md:-mt-4" : "flex flex-col w-4/6 px-2 py-2 xl:mt-24 lg:mt-24 md:mt-10"}>
                     <div className={haveWord ? "bubble grow right min-w-full" : "min-w-full bubble grow right xl:max-h-16 lg:max-h-24 md:max-h-24 "}>
