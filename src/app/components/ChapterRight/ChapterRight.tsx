a77ba0ebdd3e589ad050be6a0e07b5cafb172676
chilleno <antonion.gonzalezf@gmail.com>
2023-07-20 14:10:16 -0400

    Update ChapterRight.tsx
    - add md responsive

--- src/app/components/ChapterRight/ChapterRight.tsx
+++ src/app/components/ChapterRight/ChapterRight.tsx
@@ -56,13 +56,13 @@ const ChapterRight: React.FC<TaskListProps> = ({ id, name, url, haveWord, word, 
     return (
         <div className="w-full flex flex-row mt-24" id={'chapter_' + id}>
             <div className="flex w-1/2 content-center justify-center">
-                <div className="flex w-2/6 mt-16 xl:ml-28 xl:-mr-10 lg:-ml-10 lg:mr-10">
+                <div className="flex w-2/6 mt-16 xl:ml-28 xl:-mr-10 lg:-ml-10 lg:mr-10 md:-ml-16">
                     <img
                         className="h-[198px] w-[169px] animate-[wiggle_3s_ease-in-out_infinite]"
                         src="/logo/claudio.png" />
                 </div>
-                <div className={haveWord ? "flex flex-col w-2/4 px-2 py-6" : "flex flex-col w-2/4 px-2 py-2 mt-24"}>
-                    <div className={haveWord ? "bubble grow left min-w-full" : "min-w-full bubble grow left xl:max-h-16 lg:max-h-24"}>
+                <div className={haveWord ? "flex flex-col w-2/4 px-2 py-6" : "flex flex-col w-2/4 px-2 py-2 xl:mt-24 lg:mt-24 md:mt-10"}>
+                    <div className={haveWord ? "bubble grow left min-w-full" : "min-w-full bubble grow left xl:max-h-16 lg:max-h-24 md:max-h-24"}>
                         <div className={haveWord ? "font-outfit text-2xl mt-5" : "font-outfit text-2xl mt-2"}>
                             <b>{name}</b>
                         </div>
@@ -73,7 +73,7 @@ const ChapterRight: React.FC<TaskListProps> = ({ id, name, url, haveWord, word, 
                             {
                                 !wordMatch && haveWord &&
                                 <input
-                                    className={`border-b-2 ${showError && 'shake-error'}`}
+                                    className={`md:max-w-[180px] border-b-2 ${showError && 'shake-error'}`}
                                     placeholder="??????????????????"
                                     type="text"
                                     onChange={(e) => setCurrentTry(e.target.value)}
