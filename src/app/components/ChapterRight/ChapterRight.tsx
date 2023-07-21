0ad4d64e3c5937be45c4233a53310824314b3895
chilleno <antonion.gonzalezf@gmail.com>
2023-07-20 21:00:16 -0400

    Update ChapterRight.tsx
    - add button to validate word

--- src/app/components/ChapterRight/ChapterRight.tsx
+++ src/app/components/ChapterRight/ChapterRight.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import ConfettiExplosion from 'react-confetti-explosion';
+import { PaperAirplaneIcon } from '@heroicons/react/24/solid';
 
 interface TaskListProps {
     id: number;
@@ -72,13 +73,19 @@ const ChapterRight: React.FC<TaskListProps> = ({ id, name, url, haveWord, word, 
                         <div className="font-outfit text-2xl mt-5">
                             {
                                 !wordMatch && haveWord &&
-                                <input
-                                    className={`md:max-w-[180px] border-b-2 ${showError && 'shake-error'}`}
-                                    placeholder="??????????????????"
-                                    type="text"
-                                    onChange={(e) => setCurrentTry(e.target.value)}
-                                    onKeyDown={handlePressEnterButton}
-                                />
+                                <div className="flex flex-row">
+                                    <input
+                                        className={`w-4/6 border-b-2 ${showError && 'shake-error'}`}
+                                        placeholder="??????????????????"
+                                        type="text"
+                                        onChange={(e) => setCurrentTry(e.target.value)}
+                                        onKeyDown={handlePressEnterButton}
+                                    />
+                                    <PaperAirplaneIcon
+                                        onClick={handleWordValidation}
+                                        className="w-2/6 h-10 hover:cursor-pointer"
+                                    />
+                                </div>
                             }
                             {
                                 wordMatch &&
